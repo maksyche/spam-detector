6acfa680ba9676d563b6813551fef854e0c310ac
Maksym Chernikov <maksyche@proton.me>
2024-09-30 23:26:51 +0200

    Probably works

---
+++ neural_network_model.py
@@ -0,0 +1,165 @@
+import json
+import os
+
+import numpy as np
+
+MODEL_FILE_NAME = "./model"
+MODEL_FILE_EXTENSION = ".json"
+
+NUMBER_OF_LAYERS = 2
+
+
+class TextClassifierNetworkModel:
+    def __init__(self, layer_dimensions, embeddings_matrix, learning_rate=0.01):
+        """
+        layer_dimensions is a list that must contain the number of neurons in the respective layers: input layer
+        (flattened) and hidden layer. For example, "[2300, 128]" means that the network has 2300 neurons in the input
+        layer and 128 neurons in the hidden layer. For simplicity, the output layer always contains a single neuron.
+        Weights are initialized randomly (for hidden and output layers only) based on the number of neurons (Glorot).
+        Biases are all zeros by default. All the layers in this network are dense. The activation function is Sigmoid.
+        The loss function is the Mean Squared Error (MSE). The optimizer is the Stochastic Gradient Descent (SGD).
+        embeddings_matrix is used to convert tokenized word sequences into vector embeddings (flattened for the input
+        layer).
+
+        The goal of this code is visual simplicity, thus it's not optimized and really slow. Overall, it's a great
+        exercise, but also a great example of why it's better to use Tensorflow.
+        """
+
+        assert len(layer_dimensions) == NUMBER_OF_LAYERS
+        self.layer_dimensions = layer_dimensions
+        self.embeddings_matrix = embeddings_matrix
+        self.learning_rate = learning_rate
+        self.model_file_path = MODEL_FILE_NAME + str(layer_dimensions) + MODEL_FILE_EXTENSION
+
+        if os.access(self.model_file_path, os.R_OK):
+            print("Loading model from a file...")
+            with open(self.model_file_path, 'r') as file:
+                data = json.load(file)
+                self.weights = data[0]
+                self.biases = data[1]
+
+                # Because json doesn't deserialize anything into numpy data types
+                for layer in range(NUMBER_OF_LAYERS):
+                    self.weights[layer] = np.array(self.weights[layer])
+                    self.biases[layer] = np.array(self.biases[layer])
+
+        else:
+            print("No model file found! Initializing default weights...")
+            low_bound_h, high_bound_h = glorot_weight_bounds(layer_dimensions[0], layer_dimensions[1])
+            low_bound_o, high_bound_o = glorot_weight_bounds(layer_dimensions[1], 1)
+
+            # [(128, 2300), (128,)] for the given example
+            self.weights = [np.random.uniform(low=low_bound_h,
+                                              high=high_bound_h,
+                                              size=(layer_dimensions[1], layer_dimensions[0])),
+                            np.random.uniform(low=low_bound_o,
+                                              high=high_bound_o,
+                                              size=layer_dimensions[1])]
+
+            # [(128,), (1,)] for the given example
+            self.biases = [np.zeros(layer_dimensions[1]),
+                           np.zeros(1)]
+
+        print(self)
+
+    def __repr__(self):
+        return "Network Structure: {}".format(self.layer_dimensions)
+
+    def predict(self, validation_sequences):
+        validation_sequences = convert_to_embeddings_and_flatten(validation_sequences, self.embeddings_matrix)
+        classifications = []
+        for s in range(len(validation_sequences)):
+            ah = sigmoid(np.dot(self.weights[0], validation_sequences[s]) + self.biases[0])
+            ao = sigmoid(np.dot(self.weights[1], ah) + self.biases[1])
+            classifications.append(ao)
+        return classifications
+
+    def fit(self, training_sequences, labels):
+        training_sequences = convert_to_embeddings_and_flatten(training_sequences, self.embeddings_matrix)
+        sample_size = len(training_sequences)
+
+        der_e_w = [np.zeros((self.layer_dimensions[1], self.layer_dimensions[0])),
+                   np.zeros(self.layer_dimensions[1])]
+        der_e_b = [np.zeros(self.layer_dimensions[1]), np.zeros(1)]
+        e = 0.0
+
+        for s in range(sample_size):
+
+            # Forward pass
+            zh = np.dot(self.weights[0], training_sequences[s]) + self.biases[0]
+            ah = sigmoid(zh)
+            zo = np.dot(self.weights[1], ah) + self.biases[1]
+            ao = sigmoid(zo)
+
+            # Calculate the error
+            es = np.power(ao - labels[s], 2)
+            e += es
+
+            # Partial derivatives and backward pass
+            der_es_ao = 2.0 * (ao - labels[s])
+            der_ao_zo = sigmoid_prime(zo)
+            der_zo_wo = ao
+            der_zo_ah = self.weights[1]
+            der_ah_zh = sigmoid_prime(zh)
+            der_zh_wh = training_sequences[s]
+
+            # For the hidden layer
+            der_e_w[0] += der_zh_wh * der_ah_zh[:, None] * der_zo_ah[:, None] * der_ao_zo * der_es_ao
+            der_e_b[0] += der_ah_zh * der_zo_ah * der_ao_zo * der_es_ao
+
+            # For the output layer
+            der_e_w[1] += der_zo_wo * der_ao_zo * der_es_ao
+            der_e_b[1] += der_ao_zo * der_es_ao
+
+        # Averaging errors and adjusting the learning rate
+        print("Loss: %f" % (e / sample_size))
+        for layer in range(NUMBER_OF_LAYERS):
+            der_e_w[layer] /= sample_size
+            der_e_b[layer] /= sample_size
+
+        # Updating weights and biases
+        for layer in range(NUMBER_OF_LAYERS):
+            self.weights[layer] -= self.learning_rate * der_e_w[layer]
+            self.biases[layer] -= self.learning_rate * der_e_b[layer]
+
+        # Serializing everything to a file, so I can cancel it anytime and start from that moment
+        with open(self.model_file_path, 'w') as outfile:
+            json.dump([self.weights, self.biases], outfile, cls=NumpyEncoder)
+
+
+def glorot_weight_bounds(prev_layer_size, next_layer_size):
+    return (-(np.sqrt(6.0) / np.sqrt(prev_layer_size + next_layer_size)),
+            (np.sqrt(6.0) / np.sqrt(prev_layer_size + next_layer_size)))
+
+
+def sigmoid(z):
+    return 1.0 / (1 + np.exp(-z))
+
+
+def sigmoid_prime(z):
+    return sigmoid(z) * (1 - sigmoid(z))
+
+
+def convert_to_embeddings_and_flatten(sequences, embedding_matrix):
+    flattened_sequences = []
+    for sequence in sequences:
+        flattened = []
+        for word in sequence:
+            vector = embedding_matrix[word]
+            for value in vector:
+                flattened.append(value)
+        flattened_sequences.append(np.array(flattened))
+    return np.array(flattened_sequences)
+
+
+# JSON Numpy Encoder for serializing weights and biases
+# https://stackoverflow.com/questions/3488934/simplejson-and-numpy-array/24375113#24375113
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
---
+++ spam_detector_tensorflow.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python3
+import numpy as np
+from tf_keras.layers import Dense, Flatten
+from tf_keras.layers import Embedding
+from tf_keras.models import Sequential
+from tf_keras.optimizers.legacy import SGD
+from tf_keras.preprocessing.sequence import pad_sequences
+from tf_keras.preprocessing.text import Tokenizer
+
+# Depends on the selection of the pre-trained embedding dictionary
+EMBEDDING_VECTOR_LENGTH = 100
+
+# The percentage of dataset to use for validation (the rest is for training)
+VALIDATION_SPLIT = 0.1
+
+# How sure (in percentage) the network has to be to consider a classification of an individual sample successful
+SUCCESS_THRESHOLD = 0.9
+
+# The overall classification success threshold
+TRAINING_SUCCESS_THRESHOLD = 0.95
+
+# Load dataset
+messages = []  # list of text samples
+labels = []  # 1 spam; 0 ham
+with open('dataset.csv', newline='') as dataset_file:
+    i = 0
+    for row in dataset_file:
+        row_split = row.split(",", 1)
+        if "spam" == row_split[0]:
+            labels.append(1)
+        elif "ham" == row_split[0]:
+            labels.append(0)
+        else:
+            # Skip if it's an unknown category
+            continue
+
+        messages.append(row_split[1])
+print('Found %s texts.' % len(messages))
+
+# Make them numpy arrays and shuffle
+perm = np.random.permutation(len(messages))
+messages = np.array(messages)[perm]
+labels = np.array(labels)[perm]
+
+# Tokenize words and convert messages into sequences
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(messages)
+sequences = tokenizer.texts_to_sequences(messages)
+word_index = tokenizer.word_index
+print('Found and tokenized %s unique words.' % len(word_index))
+
+# Use pre-trained GloVe embeddings (vector length is 100): https://github.com/stanfordnlp/GloVe
+embeddings_dict = {}
+print('Loading pre-trained embeddings...')
+with open("glove.6B.100d.txt") as f:
+    for line in f:
+        word, coeffs = line.split(maxsplit=1)
+        coeffs = np.fromstring(coeffs, "f", sep=" ")
+        embeddings_dict[word] = coeffs
+print("Found %s word vectors." % len(embeddings_dict))
+
+# Prepare embedding matrix
+word_number = len(word_index) + 1  # +1 because 0 is reserved for misses
+hits = 0
+misses = 0
+embedding_matrix = np.zeros((word_number, EMBEDDING_VECTOR_LENGTH))
+for word, index in word_index.items():
+    embedding_vector = embeddings_dict.get(word)
+    if embedding_vector is not None:
+        # Words not found in embedding index (and index 0 of course) will be a vector of all zeros.
+        embedding_matrix[index] = embedding_vector
+        hits += 1
+    else:
+        misses += 1
+print("Converted %d words (%d misses)." % (hits, misses))
+
+# Split dataset on validation and training
+validation_texts_number = int(VALIDATION_SPLIT * len(sequences))
+train_sequences = sequences[:-validation_texts_number]
+validation_sequences = sequences[-validation_texts_number:]
+train_labels = labels[:-validation_texts_number]
+validation_labels = labels[-validation_texts_number:]
+
+# Padding sequences by the longest one (adds 0s for smaller sequences)
+longest_sequence = 0
+for sequence in sequences:
+    longest_sequence = longest_sequence if len(sequence) <= longest_sequence else len(sequence)
+print("Longest sequence is %s." % longest_sequence)
+padded_train_sequences = pad_sequences(train_sequences, maxlen=longest_sequence, padding='post')
+padded_validation_sequences = pad_sequences(validation_sequences, maxlen=longest_sequence, padding='post')
+
+# Build the prediction model
+model = Sequential()
+model.add(Embedding(input_dim=word_number,
+                    output_dim=EMBEDDING_VECTOR_LENGTH,
+                    weights=[embedding_matrix],
+                    input_length=longest_sequence,
+                    trainable=False))
+model.add(Flatten())
+model.add(Dense(128, activation='sigmoid'))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(optimizer=SGD(learning_rate=0.01), loss='mean_squared_error')
+model.summary()
+
+# Train the model
+epoch_count = 1
+print("Training started...")
+while True:
+    model.fit(padded_train_sequences, train_labels)
+    classifications = model.predict(padded_validation_sequences)
+
+    # I can do this better with evaluations, but ok for now
+    success_count = 0
+    for i in range(len(classifications)):
+        if classifications[i] > SUCCESS_THRESHOLD and validation_labels[i] == 1:
+            success_count += 1
+        elif classifications[i] < 1 - SUCCESS_THRESHOLD and validation_labels[i] == 0:
+            success_count += 1
+    success_rate = success_count / len(padded_validation_sequences)
+    print("----------------------------------------")
+    print("[End of epoch %d]" % epoch_count)
+    print("Success rate: %f" % success_rate)
+    print("----------------------------------------")
+    if success_rate >= TRAINING_SUCCESS_THRESHOLD:
+        break
+    epoch_count += 1
+
+# Print false classifications
+classifications = model.predict(padded_validation_sequences)
+print("---------------------------------------")
+print("[False classifications in the validation sample (sequences)]")
+for i in range(len(classifications)):
+    if ((classifications[i] > SUCCESS_THRESHOLD and validation_labels[i] == 0)
+            or (classifications[i] < 1 - SUCCESS_THRESHOLD and validation_labels[i] == 1)
+            or (SUCCESS_THRESHOLD > classifications[i] > 1 - SUCCESS_THRESHOLD)):
+        print("- " + str(tokenizer.sequences_to_texts(validation_sequences)[i]) + ": " + str(classifications[i]))
+print("---------------------------------------")
+
+# Classify a couple of samples that are not in the dataset
+print("---------------------------------------")
+print("[Manual examples]")
+samples = [
+    'Do you mind booking this hotel for our trip? Last cheap room here: https://somewebsite.com/foo',
+    'Cheap booking, last chance: https://somewebsite.com/foo. Act now before it\'s too late'
+]
+sample_sequences = tokenizer.texts_to_sequences(samples)
+padded_sample_sequences = pad_sequences(sample_sequences, maxlen=longest_sequence, padding='post')
+sample_classifications = model.predict(padded_sample_sequences)
+for i in range(len(sample_classifications)):
+    print("- " + str(samples[i]) + ": " + str(sample_classifications[i]))
